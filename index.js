bb2170551fc2dce6c65220a65aaed287b0f17f1d
peinearydevelopment <peinearydevelopment@gmail.com>
2017-03-20 15:33:29 -0400

    backup: files from tutorial given in 2014

---
+++ index.js
@@ -0,0 +1,116 @@
+﻿(function() {
+    function indexViewModel() {
+        var self = this;
+
+        self.deferredObject = ko.observable({});
+        self.deferredObjectPromise = ko.observable({});
+        self.deferredObjectState = ko.observable({});
+        self.deferredObjectStateString = ko.observable('');
+        self.handlerMessages = ko.observableArray([]);
+        createDeferred();
+
+        self.resolveDeferredObject = resolveDeferredObject;
+        self.rejectDeferredObject = rejectDeferredObject;
+        self.notifyDeferredObject = notifyDeferredObject;
+        self.hidePre = ko.observable(false);
+        self.createNewDeferred = createDeferred;
+        self.deferredObjectIsResolved = ko.pureComputed(computeObjectIsResolved);
+        self.deferredObjectIsRejected = ko.pureComputed(computeObjectIsRejected);
+        self.deferredObjectString = ko.pureComputed(function () { return createObjectString(self.deferredObject()); });
+        self.deferredObjectPromiseString = ko.pureComputed(function () { return createObjectString(self.deferredObjectPromise()); });
+        self.handlerTypes = ko.observableArray(['always', 'done', 'fail', 'progress']);
+        self.addHandlers = addHandlers;
+
+        function resolveDeferredObject() {
+            handleEvent(self.deferredObject().resolve);
+        }
+
+        function rejectDeferredObject() {
+            handleEvent(self.deferredObject().reject);
+        }
+
+        function notifyDeferredObject() {
+            handleEvent(self.deferredObject().notify, ' (after notification)');
+        }
+
+        function createDeferred() {
+            self.deferredObject(new $.Deferred());
+            self.deferredObjectPromise = ko.observable(self.deferredObject().promise());
+            handleEvent();
+            self.handlerMessages([]);
+        }
+
+        function handleEvent(func, additionalMessage) {
+            additionalMessage = additionalMessage || '';
+
+            if (func) {
+                func();
+            }
+            
+            self.deferredObjectStateString('Deferred object ' + self.deferredObject().state() + additionalMessage);
+            self.deferredObjectState(self.deferredObject().state());
+        }
+
+        function computeObjectIsResolved() {
+            return self.deferredObjectState() === 'resolved';
+        }
+
+        function computeObjectIsRejected() {
+            return self.deferredObjectState() === 'rejected';
+        }
+
+        function addHandlers() {
+            var handlerTypes = self.handlerTypes();
+            for (var i = 0, l = handlerTypes.length; i < l; i++) {
+                var handlerType = handlerTypes[i];
+                self.handlerMessages.push('Handler type: ' + handlerType + ' added.');
+                self.deferredObject()[handlerType](
+                    (function (type) {
+                        return function() {
+                            self.handlerMessages.push(type + ' invoked.');
+                        }
+                    })(handlerType)
+                );
+            }
+        }
+    }
+
+    indexViewModel.prototype.toJSON = function () {
+        var copy = this;
+
+        copy.deferredObjectString = createObjectWithFunctionsForToJsonString(copy.deferredObject);
+        copy.deferredObjectPromiseString = createObjectWithFunctionsForToJsonString(copy.deferredObjectPromise);
+
+        return copy;
+    }
+
+    function createObjectWithFunctionsForToJsonString(obj) {
+        var objString = {};
+
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop) && typeof obj[prop] === 'function')
+                objString[prop] = obj[prop].toString();
+        }
+
+        return objString;
+    }
+
+    function createObjectString(obj) {
+        var str = '{\n\r';
+
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop) && typeof obj[prop] === 'function') {
+                str += '   \'' + prop + '\': \'function () { ...}\n\r';
+            }
+            else {
+                str += '   \'' + prop + '\': \'' + obj[prop].toString() + '\n\r';
+            }
+        }
+
+        str += '}';
+
+        return str;
+    }
+
+    ko.applyBindings(new indexViewModel());
+}())
---
+++ when.js
@@ -0,0 +1,150 @@
+﻿(function () {
+    function whenViewModel() {
+        var self = this;
+
+        self.messages = ko.observableArray([]);
+        self.addDeferred = addDeferred;
+        self.whenDeferreds = ko.observableArray([]);
+        self.deferredCount = ko.pureComputed(function () { return self.whenDeferreds().length; }, this);
+        self.options = ko.observable({ allowReject: false, allowDelayResolve: false });
+        self.callWhen = callWhen;
+        self.clear = clear;
+        self.resolvedPromises = ko.observable({});
+        self.allDeferredsFailedResolved = ko.observable(true);
+        self.callResolveRemaining = callResolveRemaining;
+
+        clear();
+
+        function callResolveRemaining() {
+            var whenDeferreds = self.whenDeferreds();
+            for (var i = 0, l = whenDeferreds.length; i < l; i++) {
+                var whenDeferred = whenDeferreds[i];
+                if (whenDeferred.state() !== 'resolved' && whenDeferred.state() !== 'rejected') {
+                    whenDeferred.resolve(getRandomWaitTime());
+                }
+            }
+        }
+
+        //function allDeferredsFailedResolved() {
+        //    var allDefsFailedResolved = true;
+        //    var hasOwnProperties = false;
+        //    var resolvedPromises = self.resolvedPromises();
+        //    for (var prop in resolvedPromises) {
+        //        if (resolvedPromises.hasOwnProperty(prop)) {
+        //            hasOwnProperties = true;
+        //            var a = resolvedPromises[prop];
+        //            var b = a();
+        //            allDefsFailedResolved = allDefsFailedResolved && b;
+        //        }
+        //    }
+
+        //    self.allDeferredsFailedResolved(allDefsFailedResolved && hasOwnProperties);
+        //}
+
+        function clear() {
+            self.messages([]);
+            self.whenDeferreds([]);
+            addDeferred();
+        }
+
+        function callWhen() {
+            //http://www.tentonaxe.com/index.cfm/2011/9/22/Using-jQuerywhen-with-a-dynamic-number-of-objects
+            //$.when.apply(this, $.map(self.whenDeferreds(), function(def) {
+            //    return def;
+            //}))
+            //var funcs = new Array(self.whenDeferreds());
+            //funcs.push(allDeferredsFailedResolved);
+            //$.when.apply(this, funcs)
+            $.when.apply(this, self.whenDeferreds())
+            .then(
+                function () {
+                    self.messages.push('When then success.');
+                },
+                function () {
+                    self.messages.push('When then fail.');
+                },
+                [
+                    function () {
+                        self.messages.push('When then progress.');
+                    },
+                    function () {
+                        self.messages.push('When then progress2.');
+                    }
+                ]
+            ).done(
+                function () {
+                    self.messages.push('When done success.');
+                },
+                [
+                    function () {
+                        self.messages.push('When done fail.');
+                    },
+                    function () {
+                        self.messages.push('When done fail2.');
+                    }
+                ],
+                function () {
+                    self.messages.push('When done progress.');
+                }
+            );
+
+            $.map(self.whenDeferreds(), function (def) {
+                return invokeRandomAction(def, self.options());
+            });
+
+            self.messages.push('Outside of when finished.');
+        }
+
+        function addDeferred() {
+            self.whenDeferreds.push(createDeferred(self.deferredCount() + 1));
+        }
+
+        function createDeferred(id) {
+            var def = new $.Deferred();
+
+            self.resolvedPromises()[id] = ko.pureComputed(function() { return def.state() === 'resolved' || def.state() === 'rejected'; }, this);
+            def.done(function (timeoutWait) {
+                self.messages.push('Deferred ' + id + ' done after ' + timeoutWait + 'ms.');
+            });
+
+            def.fail(function (timeoutWait) {
+                self.messages.push('Deferred ' + id + ' rejected after ' + timeoutWait + 'ms.');
+            });
+
+            def.progress(function (timeoutWait) {
+                self.messages.push('Deferred ' + id + ' progressed after ' + timeoutWait + 'ms.');
+            });
+
+            def.always(function (timeoutWait) {
+                self.messages.push('Deferred ' + id + ' alwaysed after ' + timeoutWait + 'ms.');
+            });
+
+            return def;
+        }
+
+        function invokeRandomAction(defObject, opts) {
+            var waitTime = getRandomWaitTime();
+            var func = defObject.resolve;
+
+            if (opts.allowReject && Math.ceil(waitTime) % 3 === 0) {
+                func = defObject.reject;
+            } else if (opts.allowDelayResolve && Math.ceil(waitTime) % 3 === 1) {
+                func = defObject.notify;
+            }
+
+            invokeFuncWithWaitTime(func, waitTime);
+        }
+
+        function invokeFuncWithWaitTime(func, waitTime) {
+            setTimeout(function () {
+                func(waitTime);
+            }, waitTime);
+        }
+
+        function getRandomWaitTime() {
+            return Math.random() * 1000;
+        }
+    }
+
+    ko.applyBindings(new whenViewModel());
+}())
